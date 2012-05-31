1a3e6d274f8eaf0b9117172a2b0ebe882f422e89
Gaurav Jain <gaurav@gauravjain.org>
2012-05-31 15:37:22 -0400

    Clean up and hacked inter-procedural support
    - Cleaned up LoadStore class
    - Make use of C++ auto
    - Basic support for functions, framework is there

--- llvm/lib/Transforms/CanTM/CanTM.cpp
+++ llvm/lib/Transforms/CanTM/CanTM.cpp
@@ -28,7 +28,7 @@
 
 using namespace llvm;
 
-#define DEBUG_INFO 0
+#define DEBUG_INFO 1
 
 STATISTIC(CanTMCounter, "Counts number of functions greeted");
 
@@ -45,11 +45,15 @@ namespace {
     unsigned num_stores_unprocessed;
     unsigned num_stores_compressed;
 
-    struct LoadStore {
+    void printVal(Value *v);
+
+    class LoadStore {
         std::set<Value*> loads;
         std::set<Value*> stores;
         std::set<Value*> pred_loads;
         std::set<Value*> pred_stores;
+        
+      public:
 
         bool empty() {
             return loads.empty() && stores.empty();
@@ -68,6 +72,52 @@ namespace {
             std::pair<std::set<Value*>::iterator,bool> ret = stores.insert(v);
             return ret.second;
         }
+
+        bool compressWithPreviousLoad(Value* v);
+        bool compressWithPreviousStore(Value* v);
+        void compress(std::set<Value*> &prev_loads, std::set<Value*> &prev_stores);
+
+        //TODO: use iterator
+        void copyLoads(std::vector<Value *> &v) {
+            v.insert(v.end(), loads.begin(), loads.end());
+        }
+        void copyStores(std::vector<Value *> &v) {
+            v.insert(v.end(), stores.begin(), stores.end());
+        }
+        void copyLoads(std::set<Value *> &s) {
+            for (auto loads_it = loads.begin(), loads_it_e = loads.end(); loads_it != loads_it_e; ++loads_it) {
+                s.insert(*loads_it);
+            }
+        }
+        void copyStores(std::set<Value *> &s) {
+            for (auto stores_it = stores.begin(), stores_it_e = stores.end(); stores_it != stores_it_e; ++stores_it) {
+                s.insert(*stores_it);
+            }
+        }
+
+        unsigned numLoads() {
+            return loads.size();
+        }
+
+        unsigned numStores() {
+            return stores.size();
+        }
+
+        void debugPrint() {
+            errs() << loads.size() << " loads and " << stores.size() << " stores." << "\n";
+            errs() << "Load Set: ";
+            for (auto loads_it = loads.begin(), loads_it_e = loads.end(); loads_it != loads_it_e; ++loads_it) {
+                printVal(*loads_it);
+                errs() << " ";
+            }
+            errs() << "\n";
+            errs() << "Stores Set: ";
+            for (auto stores_it = stores.begin(), stores_it_e = stores.end(); stores_it != stores_it_e; ++stores_it) {
+                printVal(*stores_it);
+                errs() << " ";
+            }
+            errs() << "\n";
+        }
     };
 
     // CanTM - The first implementation, without getAnalysisUsage.
@@ -75,12 +125,13 @@ namespace {
         static char ID; // Pass identification, replacement for typeid
         CanTM() : ModulePass(ID) {}
 
-        void printVal(Value *v);
         void analyizeBB(BasicBlock *bb);
         void getLoadsStores(BasicBlock *bb, std::set<Value*> &loads, std::set<Value*> &stores);
-        std::map< BasicBlock *, LoadStore > bbMap;
-        std::set< Function * > fAdded;
-        std::queue< Function* > fQueue;
+        void compressFunction(Function *f);
+        std::map<BasicBlock *, LoadStore> bbMap;
+        std::set<Function *> fAdded;
+        std::set<BasicBlock *> fFunctionBlocks;
+        std::queue<Function *> fQueue;
 
         virtual bool runOnModule(Module &M);
 
@@ -93,7 +144,45 @@ char CanTM::ID = 0;
 static RegisterPass<CanTM> X("CanTM", "CanTM World Pass");
 
 
-void CanTM::printVal(Value *v) {
+bool LoadStore::compressWithPreviousLoad(Value* v) {
+    auto loads_it = loads.find(v);
+    if (loads_it != loads.end()) {
+#if DEBUG_INFO
+        errs() << "Removed a load element (due to store): ";
+        printVal(v);
+        errs() << "\n";
+#endif
+        ++num_loads_compressed;
+        loads.erase(loads_it);
+    }
+}
+
+bool LoadStore::compressWithPreviousStore(Value* v) {
+    if (compressWithPreviousLoad(v))
+        ++num_loads_compressed_from_previous_store;
+
+    auto stores_it = stores.find(v);
+    if (stores_it != stores.end()) {
+#if DEBUG_INFO
+        errs() << "Removed a store element: ";
+        printVal(v);
+        errs() << "\n";
+#endif
+        ++num_stores_compressed;
+        stores.erase(stores_it);
+    }
+}
+
+void LoadStore::compress(std::set<Value*> &prev_loads, std::set<Value*> &prev_stores) {
+    for (auto loads_it = prev_loads.begin(), loads_it_e = prev_loads.end(); loads_it != loads_it_e; ++loads_it)
+        compressWithPreviousLoad(*loads_it);
+
+    for (auto stores_it = prev_stores.begin(), stores_it_e = prev_stores.end(); stores_it != stores_it_e; ++stores_it )
+        compressWithPreviousStore(*stores_it);
+}
+
+namespace {
+void printVal(Value *v) {
     const Type* type = v->getType();
     if (type->isIntegerTy()) {
         errs() << "Integer" << type->getIntegerBitWidth() << "(";
@@ -108,11 +197,12 @@ void CanTM::printVal(Value *v) {
     }
     errs() << ")";
 }
+}
 
 void CanTM::analyizeBB(BasicBlock *bb) {
     errs() << "BB: " << bb << "\n";
     LoadStore ls;
-    for (BasicBlock::iterator instr_i = bb->begin(), instr_e = bb->end(); instr_i != instr_e; ++instr_i) {
+    for (auto instr_i = bb->begin(), instr_e = bb->end(); instr_i != instr_e; ++instr_i) {
         errs() << "Intr: ";
         if (LoadInst *li = dyn_cast<LoadInst>(&*instr_i)) {
             ++num_loads;
@@ -140,26 +230,35 @@ void CanTM::analyizeBB(BasicBlock *bb) {
             }
         } else if (CallInst *ci = dyn_cast<CallInst>(&*instr_i)) {
             errs() << "Call (" << ci->getNumArgOperands() << " args) ";
-            for (unsigned arg_num = 0; arg_num < ci->getNumArgOperands(); ++arg_num) {
-                errs() << arg_num << ": ";
-                printVal(ci->getArgOperand(arg_num));
-                errs() << " ";
-                ++num_loads;
-                ++num_loads_from_function_call;
-                if (ci->getArgOperand(arg_num)->hasName()) {
-                    if (!ls.insertLoad(ci->getArgOperand(arg_num))) {
-                        ++num_loads_skipped;
+            if (instr_i != bb->begin()) {
+                analyizeBB(bb->splitBasicBlock(instr_i));
+            } else {
+                for (unsigned arg_num = 0; arg_num < ci->getNumArgOperands(); ++arg_num) {
+                    errs() << arg_num << ": ";
+                    printVal(ci->getArgOperand(arg_num));
+                    errs() << " ";
+                    ++num_loads;
+                    ++num_loads_from_function_call;
+                    if (ci->getArgOperand(arg_num)->hasName()) {
+                        if (!ls.insertLoad(ci->getArgOperand(arg_num))) {
+                            ++num_loads_skipped;
+                        }
+                    } else {
+                        ++num_loads_unprocessed;
                     }
-                } else {
-                    ++num_loads_unprocessed;
                 }
+                fFunctionBlocks.insert(bb);
+                Function* called = ci->getCalledFunction();
+                auto it = fAdded.find(called);
+                if (it == fAdded.end()) {
+                    fQueue.push(called);
+                    fAdded.insert(called);
+                }
+                ++instr_i;
+                if (instr_i != instr_e)
+                    analyizeBB(bb->splitBasicBlock(instr_i));
             }
-            Function* called = ci->getCalledFunction();
-            std::set< Function * >::iterator it = fAdded.find(called);
-            if (it == fAdded.end()) {
-                fQueue.push(called);
-                fAdded.insert(called);
-            }
+            break;
         } else if (AllocaInst *ai = dyn_cast<AllocaInst>(&*instr_i)) {
             errs() << "Alloc";
             ++instr_i;
@@ -172,11 +271,14 @@ void CanTM::analyizeBB(BasicBlock *bb) {
         errs() << "\n";
     }
     if (!ls.empty()) {
+        errs() << "Analyized BB: " << bb << " ";
+        ls.debugPrint();
         bbMap[bb] = ls;
     }
 }
 
 void CanTM::getLoadsStores(BasicBlock *bb, std::set<Value*> &loads, std::set<Value*> &stores) {
+    errs() << "Compressing BB (begin): " << bb << "\n";
     for (pred_iterator pi = pred_begin(bb), pi_e = pred_end(bb); pi != pi_e; ++pi) {
         BasicBlock *pred = *pi;
         //errs() << "Compressing pred: " << pred << "\n";
@@ -196,59 +298,52 @@ void CanTM::getLoadsStores(BasicBlock *bb, std::set<Value*> &loads, std::set<Val
             stores = new_pred_stores;
         }
     }
+
+    errs() << "Compressing BB (middle): " << bb << "\n";
     LoadStore ls = bbMap[bb];
-    for (std::set<Value*>::iterator loads_it = ls.loads.begin(), loads_it_e = ls.loads.end(); loads_it != loads_it_e; ) {
-        if (loads.find(*loads_it) != loads.end()) {
-            ++num_loads_compressed;
-#if DEBUG_INFO
-            Value *v = *loads_it;
-            errs() << "Removed a load element: ";
-            printVal(v);
-            errs() << "\n";
-#endif
-            ls.loads.erase(loads_it);
-            loads_it = ls.loads.begin();
-            loads_it_e = ls.loads.end();
-        } else if (stores.find(*loads_it) != stores.end()) {
-            ++num_loads_compressed_from_previous_store;
-#if DEBUG_INFO
-            Value *v = *loads_it;
-            errs() << "Removed a load element (due to store): ";
-            printVal(v);
-            errs() << "\n";
-#endif
-            ls.loads.erase(loads_it);
-            loads_it = ls.loads.begin();
-            loads_it_e = ls.loads.end();
-        } else {
-            ++loads_it;
-        }
-    }
-    for (std::set<Value*>::iterator stores_it = ls.stores.begin(), stores_it_e = ls.stores.end(); stores_it != stores_it_e; ) {
-        if (stores.find(*stores_it) != stores.end()) {
-            ++num_stores_compressed;
-#if DEBUG_INFO
-            Value *v = *stores_it;
-            errs() << "Removed a store element: ";
-            printVal(v);
-            errs() << "\n";
-#endif
-            ls.stores.erase(stores_it);
-            stores_it = ls.stores.begin();
-            stores_it_e = ls.stores.end();
-        } else {
-            ++stores_it;
-        }
-    }
+    ls.compress(loads, stores);
     bbMap[bb] = ls;
-    for (std::set<Value*>::iterator loads_it = ls.loads.begin(), loads_it_e = ls.loads.end(); loads_it != loads_it_e; ++loads_it) {
+
+    if (fFunctionBlocks.find(bb) != fFunctionBlocks.end()) {
+        auto instr_i = bb->begin();
+        CallInst *ci = dyn_cast<CallInst>(&*instr_i);
+        compressFunction(ci->getCalledFunction());
+    }
+
+    ls.copyLoads(loads);
+    ls.copyStores(stores);
+   /* 
+    for (auto loads_it = ls.loads.begin(), loads_it_e = ls.loads.end(); loads_it != loads_it_e; ++loads_it) {
         loads.insert(*loads_it);
     }
-    for (std::set<Value*>::iterator stores_it = ls.stores.begin(), stores_it_e = ls.stores.end(); stores_it != stores_it_e; ++stores_it) {
+    for (auto stores_it = ls.stores.begin(), stores_it_e = ls.stores.end(); stores_it != stores_it_e; ++stores_it) {
         stores.insert(*stores_it);
-    }
+    }*/
+    errs() << "Compressed BB (end): " << bb << " ";
+    ls.debugPrint();
     //loads.insert(ls.loads.begin(), ls.loads.end());
     //stores.insert(ls.stores.begin(), ls.stores.end());
+}
+
+void CanTM::compressFunction(Function *f) {
+    for (auto arg_iter=f->arg_begin(), arg_iter_end = f->arg_end(); arg_iter != arg_iter_end; ++arg_iter) {
+        for (auto i_f = f->begin(), ie_f = f->end(); i_f != ie_f; i_f++) {
+            BasicBlock *bb = i_f;
+            LoadStore ls = bbMap[bb];
+            // TODO: FIXME For now we'll assume all arguments are properly reserved
+            ls.compressWithPreviousLoad(arg_iter);
+            ls.compressWithPreviousStore(arg_iter);
+            bbMap[bb] = ls;
+        }
+    }
+
+    // TODO: There may be multiple ending blocks
+    auto i_f = f->end();
+    --i_f;
+    BasicBlock *bb = i_f;
+    std::set<Value*> loads;
+    std::set<Value*> stores;
+    getLoadsStores(bb, loads, stores);
 }
 
 bool CanTM::runOnModule(Module &M) {
@@ -256,7 +351,7 @@ bool CanTM::runOnModule(Module &M) {
     errs() << "CanTM: ";
     stm_reserve = M.begin();
     errs().write_escaped(M.getModuleIdentifier()) << '\n';
-    for (Module::iterator i = M.begin(), ie = M.end(); i != ie; ++i) {
+    for (auto i = M.begin(), ie = M.end(); i != ie; ++i) {
         Function* f = i;
         /*
            errs() << "=========================\n";
@@ -283,7 +378,7 @@ bool CanTM::runOnModule(Module &M) {
         errs().write_escaped(f->getName()) << '\n';
         errs() << "=========================\n";
 
-        for (Function::iterator i_f = f->begin(), ie_f = f->end(); i_f != ie_f; i_f++) {
+        for (auto i_f = f->begin(), ie_f = f->end(); i_f != ie_f; i_f++) {
             BasicBlock *bb = i_f;
             analyizeBB(bb);
         }
@@ -294,46 +389,25 @@ bool CanTM::runOnModule(Module &M) {
         }
     }
 
-    // TODO: There may be multiple ending blocks
-    Function::iterator i_f = tx->end();
-    --i_f;
-    BasicBlock *bb = i_f;
-    std::set<Value*> loads;
-    std::set<Value*> stores;
-    getLoadsStores(bb, loads, stores);
+    compressFunction(tx);
 
-    for (std::map< BasicBlock *, LoadStore >::iterator it = bbMap.begin(), it_end = bbMap.end(); it != it_end; ++it) {
+    for (auto it = bbMap.begin(), it_end = bbMap.end(); it != it_end; ++it) {
         BasicBlock *bb = (*it).first;
         LoadStore ls = (*it).second;
         if (ls.empty())
             continue;
-        errs() << "Instrumenting BB: " << bb << " with " << ls.loads.size() << " loads and " << ls.stores.size() << " stores." << "\n";
-        errs() << bb << "Load Set: ";
-        for (std::set<Value*>::iterator loads_it = ls.loads.begin(), loads_it_e = ls.loads.end(); loads_it != loads_it_e; ++loads_it) {
-            printVal(*loads_it);
-            errs() << " ";
-        }
-        errs() << "\n";
-        errs() << bb << "Stores Set: ";
-        for (std::set<Value*>::iterator stores_it = ls.stores.begin(), stores_it_e = ls.stores.end(); stores_it != stores_it_e; ++stores_it) {
-            printVal(*stores_it);
-            errs() << " ";
-        }
-        errs() << "\n";
+        errs() << "Instrumenting BB: " << bb << " ";
+        ls.debugPrint();
         std::vector<Value*> args;
-        ConstantInt* num_args = ConstantInt::get(IntegerType::get(M.getContext(), 32), 2 + ls.loads.size() + ls.stores.size(), true);
+        ConstantInt* num_args = ConstantInt::get(IntegerType::get(M.getContext(), 32), 2 + ls.numLoads() + ls.numStores(), true);
         args.push_back(num_args);
-        ConstantInt* num_loads = ConstantInt::get(IntegerType::get(M.getContext(), 32), ls.loads.size(), true);
+        ConstantInt* num_loads = ConstantInt::get(IntegerType::get(M.getContext(), 32), ls.numLoads(), true);
         args.push_back(num_loads);
-        args.insert(args.end(), ls.loads.begin(), ls.loads.end());
-        ConstantInt* num_stores = ConstantInt::get(IntegerType::get(M.getContext(), 32), ls.stores.size(), true);
+        ls.copyLoads(args);
+        ConstantInt* num_stores = ConstantInt::get(IntegerType::get(M.getContext(), 32), ls.numStores(), true);
         args.push_back(num_stores);
-        args.insert(args.end(), ls.stores.begin(), ls.stores.end());
-        BasicBlock::iterator InsertPos = bb->begin();
-        /*while (isa<AllocaInst>(InsertPos)) {
-          errs() << "Shifting..." << "\n";
-          ++InsertPos;
-          }*/
+        ls.copyStores(args);
+        auto InsertPos = bb->begin();
 
         CallInst::Create(stm_reserve, args, "", InsertPos);
     }
